b4020167a933342dc5d0fb41c6c8989d8f2ffb7e
chealexander <cheshaquan@gmail.com>
2022-05-21 22:57:17 -0400

    Initial Commit

---
+++ MHDatabaseIOS/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  MHDatabaseIOS
+//
+//  Created by Che Alexander on 5/21/22.
+//
+import UIKit
+import SideMenu
+
+class ViewController: UIViewController {
+    
+    var menu: SideMenuNavigationController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
+        menu?.leftSide = true
+        
+        SideMenuManager.default.leftMenuNavigationController = menu
+        
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+    }
+    
+
+    @IBAction func didTapMenu() {
+        present(menu!, animated: true)
+    }
+
+}
+
+class MenuListController: UITableViewController {
+    
+
+    var items = ["Home", "Games", "Monster Classes", "Bookmarks", "Settings"]
+    let greyColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = greyColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        cell.backgroundColor = greyColor
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+    }
+}
---
+++ Pods/Target Support Files/Pods-MHDatabaseIOS/Pods-MHDatabaseIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MHDatabaseIOS : NSObject
+@end
+@implementation PodsDummy_Pods_MHDatabaseIOS
+@end
