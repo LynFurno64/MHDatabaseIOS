16a0a972881ba93cd3bb4bd04d7180924569bfa4
chealexander <cheshaquan@gmail.com>
2022-05-21 23:28:44 -0400

    ee

---
+++ MHDatabaseIOS/ViewControllers/DempViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DempViewController.swift
+//  MHDatabaseIOS
+//
+//  Created by Che Alexander on 5/21/22.
+//
+
+import UIKit
+
+class DempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        super.viewDidLoad()
+
+        title = "About Me"
+        view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
